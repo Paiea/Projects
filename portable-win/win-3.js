ca86ba70155a3afed2cea564a358a9143c3586c6
Paiea <paiea.tv@gmail.com>
2026-09-10 12:10:36 -1000

    feat: load teaching menu extension

--- portable-win/win-3.js
+++ portable-win/win-3.js
@@ -1,1 +1,1 @@
-document.write('<script src="win-3-1.js"><\/script><script src="win-3-2.js"><\/script><script src="win-3-3a.js"><\/script><script src="win-3-3b.js"><\/script>');
+document.write('<script src="win-3-1.js"><\/script><script src="win-3-2.js"><\/script><script src="win-3-3a.js"><\/script><script src="win-3-3b.js"><\/script><script src="teaching-menu.js"><\/script>');
